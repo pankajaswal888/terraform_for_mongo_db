3e326bc044a91e74b94281f3e36af30f57d912e3
Pankaj Aswal <89585289+pankajaswal888@users.noreply.github.com>
2022-01-11 20:55:57 +0530

    var file

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "mongdbatlas_project_id" {
+  description = "Project ID of the DB"
+  default = "Project1"
+}
+
+variable "enviroment" {
+  description = "environment to be used"
+  default = "dev"
+}
+
+variable "service_configuration" {
+  default =""
+}
